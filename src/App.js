39e5098ef0df668e654c41e9f0e04491d704d657
raghul292001 <raghulg2906@gmail.com>
2025-03-19 22:04:30 +0530

    Alert
    Add Alert component with reducer

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
+import Alert from "./components/layout/Alert";
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
       <Router>
         <div className="flex flex-col justify-between  h-screen">
           <Navbar />
           <main className="container mx-auto px-3 pb-12">
+           <Alert/>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
@@ -22,6 +26,7 @@ function App() {
           <Footer />
         </div>
       </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
